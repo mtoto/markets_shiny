89a6158640bb1811c9d5e10757955f48c560ae73
mtoto <tszilagyi@outlook.com>
2017-12-18 23:44:43 +0100

    rename to correct test

--- app2/run_tests.R
+++ app2/run_tests.R
@@ -5,6 +5,6 @@ test_that("Application works", {
         # Use compareImages=FALSE because the expected image screenshots were created
         # on a Mac, and they will differ from screenshots taken on the CI platform,
         # which runs on Linux.
-        expect_pass(testApp("/srv/shiny-server/myapp/",testnames="mytest", compareImages = FALSE))
+        expect_pass(testApp("/srv/shiny-server/myapp/", testnames = "dates", compareImages = FALSE))
 })
 
