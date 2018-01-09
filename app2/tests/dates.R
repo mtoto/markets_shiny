86960e263b3faf9bdf2775ba2a8f93bd74c05a83
mtoto <tszilagyi@outlook.com>
2018-01-09 23:40:15 +0100

    test runs, diff output fix 1

--- app2/tests/dates.R
+++ app2/tests/dates.R
@@ -5,4 +5,4 @@ app$snapshotInit("dates")
 
 app$setInputs(date1 = "2000-10-02", timeout_ = 15000)
 app$setInputs(date2 = "2013-11-01", timeout_ = 15000)
-app$snapshot(items = list(input = TRUE, output = TRUE))
+app$snapshot()
